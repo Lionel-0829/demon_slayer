5d219adbd6d6d92760e3f52988c389c99edf5798
Nayla <naylagomezarguello@gmail.com>
2025-09-07 06:27:41 -0300

    Array

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import Main from '../Main/Main';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <>
+      <footer>
+
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
